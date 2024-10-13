f0e87d6e003980a32dd2136081c853b867833b99
Volodymyr Skochko <vovaskochko@gmail.com>
2024-10-14 00:13:33 +0300

    Basic support of Quartus on MacOS

---
+++ macos_helper_quartus.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+ACCOUNT_NAME=user
+ACCOUNT_PASSWORD=user
+VM_NAME=quartus
+
+PATH_TO_VNC_PASS="$(realpath ~/.lima)/$VM_NAME/vncpassword"
+GREEN_COLOR='\033[0;32m'
+YELLOW_COLOR='\033[0;33m'
+RED_COLOR='\033[0;31m'
+END_COLOR='\033[0m'
+
+# Check whether VM already exists and exit if so:
+if [ -d ~/.lima/$VM_NAME ]; then
+    echo -e "${RED_COLOR}$VM_NAME virtual machine is already exist.$END_COLOR"
+    echo -e "${RED_COLOR}To run the script delete ${VM_NAME} VM manually:${END_COLOR}"
+    echo -e "${YELLOW_COLOR}    limactl delete -f $VM_NAME${END_COLOR}"
+    exit 1
+fi
+
+# Install lima and create a new VM:
+brew install lima wget
+mkdir ~/.lima/$VM_NAME
+cp lima.yaml.tmpl ~/.lima/$VM_NAME/lima.yaml
+
+limactl start $VM_NAME
+
+# Add new user account to VM:
+limactl shell $VM_NAME -- sudo adduser --gecos "" --disabled-password "$ACCOUNT_NAME"
+limactl shell $VM_NAME -- sudo chpasswd <<<"$ACCOUNT_NAME:$ACCOUNT_PASSWORD"
+limactl shell $VM_NAME -- sudo usermod -aG sudo "$ACCOUNT_NAME"
+
+# For aarch64 Ubuntu we need to use qemu-user-static to run the x86_64 Quartus software.
+# But due to the bug https://github.com/containers/podman/discussions/22714
+# we can't use version from Ubuntu 24.04 repository therefore let's download older version from Ubuntu 23.10:
+if [ "$(uname -m)" = "arm64" ]; then
+    QEMU_DEB=qemu-user-static_8.0.4+dfsg-1ubuntu3.23.10.5_arm64.deb
+    QEMU_DEB_URL=http://launchpadlibrarian.net/721385889/$QEMU_DEB
+    for i in {1..5}; do
+        wget $QEMU_DEB_URL -O /tmp/lima/$QEMU_DEB
+        if [ "$?" -eq 0 ]; then
+            break
+        fi
+        echo -e "${YELLOW_COLOR}Failed to download $QEMU_DEB_URL, retrying...${END_COLOR}"
+        sleep 5
+    done
+    if [ ! -f "/tmp/lima/$QEMU_DEB" ] || [ "$(du -s /tmp/lima/$QEMU_DEB | awk '{print $1}')" -eq 0 ]; then
+        echo -e "${RED_COLOR}Failed to download $QEMU_DEB_URL${END_COLOR}"
+        echo -e "${RED_COLOR}Please check your internet connection and try again.${END_COLOR}"
+        exit 1
+    fi
+    limactl shell $VM_NAME -- sudo dpkg -i /tmp/lima/$QEMU_DEB
+    limactl shell $VM_NAME -- sudo bash -c "echo 'qemu-user-static hold' | sudo dpkg --set-selections"
+fi
+
+# apt sources should be patched to support amd64 and i386 binaries:
+cp ubuntu.sources /tmp/lima
+limactl shell $VM_NAME -- sudo cp /tmp/lima/ubuntu.sources /etc/apt/sources.list.d
+limactl shell $VM_NAME -- sudo bash -c "echo 'apt_preserve_sources_list: true' >> /etc/cloud/cloud.cfg"
+limactl shell $VM_NAME -- sudo dpkg --add-architecture amd64
+limactl shell $VM_NAME -- sudo dpkg --add-architecture i386
+limactl shell $VM_NAME -- sudo apt update -y
+limactl shell $VM_NAME -- sudo apt upgrade -y
+
+# Install xfce desktop, lightdm, Quartus dependencies and firefox:
+limactl shell $VM_NAME -- sudo DEBIAN_FRONTEND=noninteractive apt install -y \
+        libxft2:i386 libxext6:i386 libncurses6:i386 \
+        bzip2:i386 g++-multilib libglibd-2.0-0:amd64 \
+        libfreetype6:amd64 libsm6:amd64 libxrender1:amd64 \
+        libfontconfig1:amd64 libxext6:amd64 libcrypt1:amd64
+limactl shell $VM_NAME -- sudo DEBIAN_FRONTEND=noninteractive apt install -y \
+        xfce4 xfce4-goodies xfce4-session lightdm language-pack-uk
+limactl shell $VM_NAME -- sudo apt autoremove -y
+limactl shell $VM_NAME -- sudo apt install -y chromium-browser
+
+# Tune configuration to ensure that xfce will be used as a default desktop environment:
+limactl shell $VM_NAME -- sudo mkdir -p /var/lib/AccountsService/users/
+limactl shell $VM_NAME -- sudo bash -c "echo -e '[org.freedesktop.DisplayManager.AccountsService]\nBackgroundFile='/usr/share/backgrounds/xfce/xfce-shapes.svg'\n\n[User]\nSession=xfce\nXSession=xfce\nIcon=/home/user/.face\nSystemAccount=false' > /var/lib/AccountsService/users/$ACCOUNT_NAME"
+
+# Download quartus installer inside VM and run it:
+cp ubuntu_quartus_installer.sh /tmp/lima
+limactl shell $VM_NAME -- chmod +x /tmp/lima/ubuntu_quartus_installer.sh
+limactl shell $VM_NAME -- sudo -u $ACCOUNT_NAME /tmp/lima/ubuntu_quartus_installer.sh
+limactl shell $VM_NAME -- sudo -u $ACCOUNT_NAME ln -s /QuartusVM /home/$ACCOUNT_NAME/QuartusVM
+
+limactl stop $VM_NAME
+mkdir -p ~/QuartusVM
+sed -i '' 's,#SHARED_FOLDER,  - location\: "~/QuartusVM"\n    mountPoint\: "/QuartusVM"\n    writable: true,g' "$HOME/.lima/$VM_NAME/lima.yaml"
+limactl start $VM_NAME 
+
+echo -e "\n==========================="
+echo -e "Use ${GREEN_COLOR}Screen Sharing${END_COLOR} tool with parameters:"
+echo -e "   Address: ${GREEN_COLOR}vnc://127.0.0.1:5900${END_COLOR}"
+echo -e "   VNC Password: ${GREEN_COLOR}$(cat ${PATH_TO_VNC_PASS})${END_COLOR}"
+echo -e "   Ubuntu login: ${GREEN_COLOR}$ACCOUNT_NAME${END_COLOR}"
+echo -e "   Ubuntu password: ${GREEN_COLOR}$ACCOUNT_PASSWORD${END_COLOR}"
+echo -e "NOTE: VNC password is stored on your Mac inside ${GREEN_COLOR}${PATH_TO_VNC_PASS}${END_COLOR}"
+echo -e "===========================\n"
---
+++ ubuntu_quartus_installer.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+QUARTUS_LINK="https://cdrdv2.intel.com/v1/dl/getContent/660904/660963?filename=Quartus-lite-20.1.1.720-linux.tar"
+QUARTUS_ARCHIVE="$(realpath ~)/quartus_20.1.1.tar"
+INSTALL_DIR="$(realpath ~)/intelFPGA_lite/20.1"
+QUARTUS_TMP_DIR=~/quartus
+
+# Download installer archive:
+echo "Downloading Quartus Lite installer..."
+for i in {1..5}; do
+    wget "$QUARTUS_LINK" -O "$QUARTUS_ARCHIVE"
+    if [ "$?" -eq 0 ]; then
+        break
+    fi
+    echo -e "ERROR: Failed to download $QUARTUS_ARCHIVE, retrying..."
+    sleep 5
+done
+
+# Extract archive to the folder and remove archive file to save some space:
+echo "Extracting Quartus Lite installer..."
+mkdir "$QUARTUS_TMP_DIR"
+tar xvf "$QUARTUS_ARCHIVE" -C "$QUARTUS_TMP_DIR"
+rm "$QUARTUS_ARCHIVE"
+
+# We need to remove unneeded boards and installers to save some space:
+echo "Removing unneeded components from installer..."
+cd "${QUARTUS_TMP_DIR}/components"
+rm -rf QuartusHelpSetup-20.1.1.720-linux.run arria_*.qdz max*.qdz cyclonev*.qdz cyclone10*.qdz
+
+# Install Quartus Lite and board config:
+echo "Installing Quartus Lite..."
+./QuartusLiteSetup-20.1.1.720-linux.run --mode unattended --unattendedmodeui minimal --accept_eula 1 --installdir $INSTALL_DIR
+rm ./QuartusLiteSetup-20.1.1.720-linux.run
+
+# Install Model Simulator:
+echo "Installing Model Simulator..."
+./ModelSimSetup-20.1.1.720-linux.run --mode unattended --unattendedmodeui minimal --accept_eula 1 --installdir $INSTALL_DIR
+
+# Remove temporary directory:
+rm -rf "${QUARTUS_TMP_DIR}"
+
+# Patch Quartus startup environment script for aarch64 Ubuntu:
+if [ "$(uname -m)" = "aarch64" ]; then
+    QENV_FILE="$INSTALL_DIR/quartus/adm/qenv.sh"
+    sed -i 's/x86_64/aarch64/g' "$QENV_FILE"
+    sed -i 's/"$cpumodel"/"Nehalem"/g' "$QENV_FILE"
+    sed -i 's/grep sse/grep fp/g' "$QENV_FILE"
+fi
+
+# There is an issue with extra option -novopt in Model Simulator - it was added by default but it blocks simulation start.
+# We can just patch corresponding library with substitution of that symbols with the same amount of spaces.
+# Spaces are required to ensure that all the relative addresses will be the same after replace.
+FILE=${INSTALL_DIR}/quartus/linux64/libedt_wedtq.so
+if [ ! -f "${FILE}orig" ]; then cp "${FILE}" "${FILE}orig"; sed -i 's/-novopt /        /g' "${FILE}"; fi
+
+# Add shortcut to Desktop to run Quartus:
+mkdir -p ~/Desktop
+echo '#!'"""/usr/bin/env xdg-open
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Exec=./quartus
+Path=${INSTALL_DIR}/quartus/bin/
+Icon=${INSTALL_DIR}/quartus/common/tcl/packages/dse/pjm.gif
+Name=Quartus Prime Lite
+Comment=Launch Quartus Prime Lite
+""" > ~/Desktop/Quartus.desktop
+chmod +x ~/Desktop/Quartus.desktop
+
+# Add shortcut to kill Quartus in case of hang:
+echo -e '#!'"/bin/bash\n\nkill -9 \$(ps aux | grep quartus\$ | awk '{print $2}')\n" > "${INSTALL_DIR}/quartus/kill_quartus.sh"
+chmod +x "${INSTALL_DIR}/quartus/kill_quartus.sh"
+
+echo '#!'"""/usr/bin/env xdg-open
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=true
+Exec="${INSTALL_DIR}/quartus/kill_quartus.sh"
+Icon=${INSTALL_DIR}/quartus/kill_quartus.sh
+Name=Kill Quartus
+Comment=Kill Quartus Prime Lite
+""" > ~/Desktop/KillQuartus.desktop
+chmod +x ~/Desktop/KillQuartus.desktop
+
+
+# Just report installation success:
+echo "Quartus Prime Lite and Model Simulator was installed successfully."
